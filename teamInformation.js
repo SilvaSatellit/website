4e896d9e0d826a206b70ef92ec1a87948a55cde2
Jonathan Westphal <79211593+ojj2008@users.noreply.github.com>
2024-02-04 20:26:21 +0100

    Update teamInformation.js

--- teamInformation.js
+++ teamInformation.js
@@ -1,20 +1,46 @@
-personen = document.querySelectorAll(".personInfo")
-
-function handleInformation(person, event) {
-  if (event === "show") {
-    personen[person-1].style.display = "block";
-  }
-  if (event === "disappear") {
-    personen[person-1].style.display = "none";
-  }
-}
-personen = document.querySelectorAll(".personInfo")
-
-function handleInformation(person, event) {
-  if (event === "show") {
-    personen[person-1].style.display = "block";
-  }
-  if (event === "disappear") {
-    personen[person-1].style.display = "none";
-  }
-}
+personen = document.querySelectorAll(".personInfo")
+
+
+
+function handleInformation(person, event) {
+
+  if (event === "show") {
+    personen[person-1].style.display = "block";
+  }
+
+  if (event === "disappear") {
+    personen[person-1].style.display = "none";
+  }
+
+}
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var popup = document.getElementById('anzeige-interaktiv');
+    var model = document.getElementById('three-js-model');
+    var viewed = false;
+
+    function startTimer() {
+      setTimeout(function() {
+        popup.style.opacity = '0';
+        setTimeout(function() {
+          popup.style.display = 'none';
+        }, 500);
+      }, 3000);
+    }
+
+    popup.addEventListener("mouseenter", function() {
+      if (!viewed) {
+        viewed = true;
+        startTimer();
+      }
+    });
+
+    model.addEventListener("mouseenter", function() {
+      if (!viewed) {
+        viewed = true;
+        startTimer();
+      }
+    });
+  });
+
