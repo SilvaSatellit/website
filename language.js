de93653bc1ed9bc0514205533de48d8eb086ba9a
Jonathan Westphal <79211593+ojj2008@users.noreply.github.com>
2024-02-05 16:06:21 +0100

    Update language.js
    Updated the english translation.

--- language.js
+++ language.js
@@ -17,7 +17,7 @@ svg_english.style.display = "block";
 
 //Text Mission Deutsch
 mission.querySelector('h2').textContent = "Mission";
-mission.querySelector('p').textContent = "Unsere Sekundärmission bestand darin, die Oberflächentemperatur des Waldes mithilfe einer Wärmebildkamera am CanSat zu erfassen und gleichzeitig die relative Luftfeuchtigkeit an zentralen Stellen im Wald zu messen. Dies sollte der frühzeitigen Erkennung von gefährdeten Waldgebieten dienen.";
+mission.querySelector('p').textContent = "Unsere Sekundärmission besteht darin, die Oberflächentemperatur des Waldes mithilfe einer Wärmebildkamera am CanSat zu erfassen und gleichzeitig die relative Luftfeuchtigkeit an zentralen Stellen im Wald zu messen. Dies sollte der frühzeitigen Erkennung von gefährdeten Waldgebieten dienen.";
 
 //Text Vorstellung Deutsch
 vorstellung.querySelector('h2').textContent = "Vorstellung";
@@ -39,16 +39,16 @@ svg_english.style.display = "none";
 //console.log("English")
 
 //Text Mission Englisch
-mission.querySelector('h2').textContent = "Neue Überschrift";
-mission.querySelector('p').textContent = "Neuer Text für die Mission. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+mission.querySelector('h2').textContent = "Mission";
+mission.querySelector('p').textContent = "Our secondary mission is about capturing the surface temperature of the forest using a thermal imaging camera attached to the CanSat, while simultaneously measuring the relative humidity at central locations within the forest. This was intended for the early detection of forest areas, at high risk of wild fires.";
 
 //Text Vorstellung Englisch
-vorstellung.querySelector('h2').textContent = "Neue Vorstellung";
-vorstellung.querySelector('h3:nth-of-type(1)').textContent = "Neuer Titel für den CanSat Wettbewerb";
-vorstellung.querySelector('.splitInfo:nth-of-type(1) p').textContent = "Neuer Text für den CanSat Wettbewerb. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-vorstellung.querySelector('h3:nth-of-type(2)').textContent = "Neuer Titel für das Life-Science Lab";
-vorstellung.querySelector('.splitInfo:nth-of-type(2) p').textContent = "Neuer Text für das Life-Science Lab. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-vorstellung.querySelector('h3:nth-of-type(3)').textContent = "Neuer Titel";
+vorstellung.querySelector('h2').textContent = "Introduction";
+vorstellung.querySelector('h3:nth-of-type(1)').textContent = "The CanSat Competition";
+vorstellung.querySelector('.splitInfo:nth-of-type(1) p').innerHTML = "Using a can-sized satellite, known as " + "<a href='https://www.cansat.de/'>" + "CanSat" + "</a>" + " in English, we test and model the process of a real space mission. We have already successfully qualified for the German CanSat Competition, where we will present our results. This competition will take place from March 11. to March 15. 2024, in Bremen.";
+vorstellung.querySelector('h3:nth-of-type(2)').textContent = "The Life-Science Lab";
+vorstellung.querySelector('.splitInfo:nth-of-type(2) p').innerHTML = "Our team consists of participants from the extracurricular program " + "<a href='https://www.life-science-lab.org/cms/'>" + "Heidelberger Life-Science Lab" + "</a>" + ", an independent department at the German Cancer Research Center that provides ideal support for science enthusiasts in middle and high school up to graduation.";
+vorstellung.querySelector('h3:nth-of-type(3)').textContent = "Our Core Team";
 
 
 // Texte interaktives Modell Englisch
